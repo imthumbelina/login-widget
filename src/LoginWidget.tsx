d46fec46c260a51611ce567e28af8ea9b6c025b0
Agnieszka Pierzchała <aga@Agnieszkas-MacBook-Air.local>
2024-04-25 07:56:52 +0200

    feat: add login widget

---
+++ src/LoginWidget.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { useForm, SubmitHandler } from "react-hook-form"
+import './LoginWidget.css';
+
+
+type FormValues = {
+  email: string
+  password: string
+}
+
+export default function LoginWidget() {      
+  const { register, handleSubmit,     formState // here
+} = useForm<FormValues>({ mode: "onChange"})
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    localStorage.setItem("email", JSON.stringify(data.email));
+    console.log(data)
+}
+
+  return (
+    <div>
+    <p>Log in</p>
+    <div className="Form-container">
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Email</label>
+      <input type="email" {...register("email", { required: true,           pattern: {
+            value: /\S+@\S+\.\S+/,
+            message: "Entered value does not match email format",
+          }, })} placeholder="Email address" />
+      <label>Password</label>
+      <input type="password" {...register("password", { required: true, minLength: 8 })} placeholder="Password"/>
+      <input type="submit" value="Log in" disabled={!formState.isValid} />
+    </form>
+    </div>
+    </div>
+    
+  )
+}
